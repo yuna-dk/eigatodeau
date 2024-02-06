5f7108f528a828d017aff454ddbac67b5698b1fd
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-02-06 01:52:51 +0000

    [Add]映画詳細画面未完

--- app/controllers/public/post_movies_controller.rb
+++ app/controllers/public/post_movies_controller.rb
@@ -16,9 +16,12 @@ class Public::PostMoviesController < ApplicationController
 
   def index
     @post_movies = PostMovie.all
+    @post_movies = PostMovie.page(params[:page]).per(6)
   end
 
   def show
+    @post_movie = PostMovie.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def edit
--- app/models/post_movie.rb
+++ app/models/post_movie.rb
@@ -7,7 +7,15 @@ class PostMovie < ApplicationRecord
   validates :image, presence: true
   validates :impression, presence: true
 
-  def get_image(width, height)
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+
+  def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
