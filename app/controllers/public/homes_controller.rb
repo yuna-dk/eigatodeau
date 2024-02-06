2756822bdca1876e73d218a861e5ebad575c496d
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-02-06 07:34:54 +0000

    [Finish]TOPページ終了

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,5 @@
 class Public::HomesController < ApplicationController
   def top
+    @post_movies = PostMovie.order(created_at: :desc).limit(4)
   end
 end
--- app/models/post_movie.rb
+++ app/models/post_movie.rb
@@ -15,12 +15,12 @@ class PostMovie < ApplicationRecord
     end
   end
 
-  def get_image
+  def get_image(width,height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
